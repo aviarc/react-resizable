a5ef5f477c200134c2912b0337e8dd5051f9d7cf
technocrusaders <nicgra@gmail.com>
2017-09-08 18:32:33 +1200

    Huage refactor!

--- test/TestLayout.js
+++ test/TestLayout.js
@@ -1,61 +1,60 @@
 import * as React from 'react';
 import Resizable from '../lib/Resizable';
-import ResizableBox from '../lib/ResizableBox';
 import 'style-loader!css-loader!../css/styles.css';
 
-export default class TestLayout extends React.Component<{}, {width: number, height: number}> {
-  state = {width: 200, height: 200};
+export default class TestLayout extends React.Component<{}, { width: number, height: number }> {
+    state = {
+        width: 150,
+        height: 150,
+        left: 200,
+        top: 300
+    };
 
-  onClick = () => {
-    this.setState({width: 200, height: 200});
-  };
 
-  onResize = (event, {element, size}) => {
-    this.setState({width: size.width, height: size.height});
-  };
+    onClick = () => {
+        this.setState({width: 150, height: 150});
+    };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onClick} style={{'marginBottom': '10px'}}>Reset first element's width/height</button>
-        <div className="layoutRoot">
-          <Resizable className="box" height={this.state.height} width={this.state.width} onResize={this.onResize}>
-            <div className="box" style={{width: this.state.width + 'px', height: this.state.height + 'px'}}>
-              <span className="text">{"Raw use of <Resizable> element. 200x200, no constraints."}</span>
+    onResize = (event, {element, size}) => {
+        this.setState(
+            {
+                width: size.width,
+                height: size.height,
+                left: size.left,
+                top: size.top
+            }
+        );
+    };
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.onClick} style={{'marginBottom': '10px'}}>Reset first element's width/height
+                </button>
+
+                <div className="layoutRoot">
+
+                    <Resizable minConstraints={[100, 120]} maxConstraints={[250, 250]} className="box"
+                               height={this.state.height} width={this.state.width}
+                               onResize={this.onResize}>
+                        <div style={{
+                            position: 'absolute',
+                            overflow: 'hidden',
+                            left: this.state.left + 'px',
+                            top: this.state.top + 'px',
+                            width: this.state.width + 'px',
+                            height: this.state.height + 'px',
+                            border: '1px solid red',
+                            backgroundColor:'green'
+                        }}>XXXXXXXXXX
+                            XXasdasdasdasdassdddddddddddd
+                            asffffffffffffffffff
+                            fffffffffffffffffffffasfasdfsdf
+                            asdfsdgfsdgfsdg
+                        </div>
+                    </Resizable>
+                </div>
             </div>
-          </Resizable>
-          <ResizableBox className="box" width={200} height={200}>
-            <span className="text">{"<ResizableBox>, same as above."}</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={200} draggableOpts={{grid: [25, 25]}}>
-            <span className="text">Resizable box that snaps to even intervals of 25px.</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={200} minConstraints={[150, 150]} maxConstraints={[500, 300]}>
-            <span className="text">Resizable box, starting at 200x200. Min size is 150x150, max is 500x300.</span>
-          </ResizableBox>
-          <ResizableBox className="box box3" width={200} height={200} minConstraints={[150, 150]} maxConstraints={[500, 300]}>
-            <span className="text">Resizable box with a handle that only appears on hover.</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={200} lockAspectRatio={true}>
-            <span className="text">Resizable square with a locked aspect ratio.</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={120} lockAspectRatio={true}>
-            <span className="text">Resizable rectangle with a locked aspect ratio.</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={200} axis="x">
-            <span className="text">Only resizable by "x" axis.</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={200} axis="y">
-            <span className="text">Only resizable by "y" axis.</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={200} axis="both">
-            <span className="text">Resizable ("both" axis).</span>
-          </ResizableBox>
-          <ResizableBox className="box" width={200} height={200} axis="none">
-            <span className="text">Not resizable ("none" axis).</span>
-          </ResizableBox>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
