0f669afc844069436ad15c2e71f12969817851d3
Daniil Moskovtsov <qa000100@gmail.com>
2017-09-29 15:48:25 +1300

    PNG-126 Fix top/left calculation.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 module.exports = function() {
-  throw new Error("Don't instantiate Resizable directly! Use require('react-resizable').Resizable");
-};
+  throw new Error('Don\'t instantiate Resizable directly! Use require(\'react-resizable\').Resizable')
+}
 
-module.exports.Resizable = require('./build/Resizable').default
-module.exports.ResizableBox = require('./build/ResizableBox').default
+module.exports.Resizable = require('./lib/Resizable').default
+module.exports.ResizableBox = require('./lib/ResizableBox').default
